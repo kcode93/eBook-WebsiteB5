5372f5708c0a3e558623fa78c7e2d8212af34b9e
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-01-11 22:15:29 -0800

    if applied, this commit adds a transparent effect to navbar on scroll

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -1,1 +1,17 @@
-//Custom JS Here
+//Add background to navbar on Scroll
+function navbarStyleOnScroll(){
+    const windowPositionTrigger = 50;
+    const navbar = document.querySelector('#main-navbar');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > windowPositionTrigger){
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-transparent');
+        }else{
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-transparent');
+        }
+    }); 
+}
+
+document.addEventListener('DOMContentLoaded', navbarStyleOnScroll);
